177779849595a40e8f6c5fc4b2babd8d5fce29d6
manarsaighi <manarsaighi2003@icloud.com>
2024-10-16 15:20:23 +0100

    home

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ function App() {
   return (
     <div className="App">
       <div class="vertical-menu">
-        <h1 class="title">xxxxx</h1>
         <a href="#" class="active">Home</a>
         <a href="#">About</a>
         <a href="#">Projects</a>
         <a href="#">Contact</a>
       </div>
+      <h1 class="title">M Saighi</h1>
     </div>
+    
   );
 }
 
