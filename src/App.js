a629fca35521560304d530638eba7923bf01aa84
manarsaighi <manarsaighi2003@icloud.com>
2024-10-18 18:51:12 +0100

    some css and  mobile repsonsiveness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
-import Modal from './Modal';  // Import the Modal component
+import Modal from './Modal';  
 
 function App() {
   const [activeModal, setActiveModal] = useState(null);
@@ -16,71 +16,71 @@ function App() {
   return (
     <div className="App">
       <div id="home">
-        {/* <div className="nav">
-          <a onClick={() => openModal(4)} className="active">Resumé</a>
-        </div> */}
+
 
         <div className="intro">
           <p>Hello, I am an undergraduate student currently pursuing my BSc in Computer Science at the Technological University of Dublin. Welcome to my page.</p>
           <p>I am actively seeking internship or entry-level job opportunities. <a href="mailto:manarsaighi2003@icloud.com">Get in Touch!</a></p>
         </div>
 
-        <div>
-          <h1 className="title">msai</h1>
-          {/* <h6>Work in progress... Last updated 17/10/24</h6> */}
-        </div>
-        
-
-        {/* Projects Section */}
+      
         <div id="projects">
           <div id="proj" onClick={() => openModal(1)}>
-            <h1>i. ABSTRACT MUSIC VISUALISER</h1>
+         
+          <h1>i. ABSTRACT MUSIC VISUALISER <span id="date">2023</span></h1>
+          
+          
             <h3>Group Project, Java</h3>
             <p>An abstract visual representation of a chosen song using a combination of synchronizing 2D and 3D animations.</p>
           </div>
         
 
           <div id="proj" onClick={() => openModal(2)}>
-            <h1>ii. GENETIC ALGORITHM </h1>
+           
+            <h1>ii. GENETIC ALGORITHM <span id="date">2024</span></h1>
             <h3>Personal Project, Java</h3>
             <p>A genetic algorithm to find the optimal folding of an amino acid sequence using the 2D HP model. Visualises the phenotype of a given solution candidate.</p>
           </div>
           
 
           <div id="proj" onClick={() => openModal(3)}>
-            <h1>iii. DOT NET MAUI APPLICATION</h1>
+            <h1>iii. DOT NET MAUI APPLICATION <span id="date">2024</span></h1>
             <h3>Personal Project, C#</h3>
             <p>A book tracking and rating desktop application. Implemented book tracking, reviewing, and recommendation features. Utilized Entity Framework for database management and deployed on Azure for cloud integration.</p>
           </div>
         
 
           <div id="proj" onClick={() => openModal(5)}>
-            <h1>iv. GAME LOGGING WEBSITE</h1>
+            <h1>iv. GAME LOGGING WEBSITE <span id="date">2023</span></h1>
             <h3>Personal Project, React, Node.js</h3>
             <p>A book tracking and rating desktop application. Implemented book tracking, reviewing, and recommendation features. Utilized Entity Framework for database management and deployed on Azure for cloud integration.</p>
           </div>
       
 
           <div id="proj" onClick={() => openModal(6)}>
-            <h1>v. PRIM, KRUSKAL, DIJKSTRA</h1>
+            <h1>v. PRIM, KRUSKAL, DIJKSTRA <span id="date">2023</span></h1>
             <h3>Personal Project, C</h3>
             <p>A book tracking and rating desktop application. Implemented book tracking, reviewing, and recommendation features. Utilized Entity Framework for database management and deployed on Azure for cloud integration.</p>
           </div>
           
           <div id="proj" onClick={() => openModal(7)}>
-            <h1>vi. VAPESAVER</h1>
+            <h1>vi. VAPESAVER <span id="date">2024</span></h1>
             <h3>Group Project, C#</h3>
             <p>Collaborated remotely with classmates during a study exchange to develop a vape cost tracking app. Presented weekly progress to a client and a panel of lecturers. Utilized Java and Firebase for backend storage and cloud services.</p>
           </div>
-      
-
-          <div id="proj" onClick={() => openModal(3)}>
-            <h1>vii. DotNet Maui Desktop Application</h1>
-            <h3>Personal Project, C#</h3>
-            <p>A book tracking and rating desktop application. Implemented book tracking, reviewing, and recommendation features. Utilized Entity Framework for database management and deployed on Azure for cloud integration.</p>
-          </div>
          
         </div>
+
+        <div className="contact">
+          <a href="https://github.com/manarsaighi" className="active">Github</a>
+          <a href="https://www.linkedin.com/in/manarsaighi/" className="active">LinkedIn</a>
+        </div>
+
+        <div>
+          <h1 className="title">SAIGHI</h1>
+        </div>
+
+       
 
 
         {/* Modal Components for Different Projects */}
@@ -128,13 +128,9 @@ function App() {
           )}        
           />
 
-        {/* Contact Section */}
-        <div className="contact">
-          <a href="https://github.com/manarsaighi" className="active">Github</a>
-          <a href="https://www.linkedin.com/in/manarsaighi/" className="active">LinkedIn</a>
-        </div>
-
         
+        
+
       </div>
     </div>
   );
